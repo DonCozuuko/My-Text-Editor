45f8ff548fd157b43987c677720e87637c1b7158
Don Cozuuko <leojamband@gmail.com>
2025-05-25 18:37:54 -0400

    Added the ability to save current content/text input on screen to a text file in the same directory as the .exe. Also when you close the executable window, it saves the last text input to that same text file. Cleaned up some of the formattable keys, apparently raylib can only print ascii keys, particularily the (char)keys call prints garbage values when NOT(key >= 32 && key <= 127).

--- editor.c
+++ editor.c
@@ -4,11 +4,12 @@
 
 static const int screenWidth = 960;
 static const int screenHeight = 540;
-static const int fpsLim = 60;
+static const int fpsCap = 60;
 static const int maxInputChars = 100;
 static const float fontSize = 25.0;
 static const float fontSpacing = 2.0;
-static const int blinkRate = 40;
+static const int blinkRate = 40;            // how fast the cursor blinks
+static const int backspaceBuffer = 25;      // how long it takes to hold down backspace to rapidly backspace
 
 void updateShiftState(int key, int *shiftToggle) {
     if (key == 340) {
@@ -34,16 +35,17 @@ void formatChars(char inputBuffer[], int *numChars, int key) {
 
 void handleCharacterInput(int key, char inputBuffer[], int *numChars, int *shiftToggle) {
     if (*shiftToggle == 0) {
-        // handles lower-case letters
-        if (key >= 65 && key <= 90) {
+        // Handles keys not on the shift layer
+        if (key >= KEY_A && KEY_Z <= 90) {
             key += 32;
         }
     }
     else {
+        // If shift button is being held
         handleSpecialChars(&key);
     }
-    // Add the keys to the string array
-    if (key != 340) {
+    // Add the keys to the string array, but the keys must be printable ascii keys which excludes shift and cntrl
+    if (key >= 32 && key <= 127) {
         formatChars(inputBuffer, numChars, key);
     }
 }
@@ -52,7 +54,7 @@ void handleKeyPress(int key, char inputBuffer[], int *numChars, int *beginDispla
     // Handles rapid deleting with a counter buffer
     if (IsKeyDown(KEY_BACKSPACE)) {
         delTime[0]++;
-        if (delTime[0] > 25) {
+        if (delTime[0] > backspaceBuffer) {
             handleBackspace(inputBuffer, numChars);
         }
     }
@@ -64,15 +66,18 @@ void handleKeyPress(int key, char inputBuffer[], int *numChars, int *beginDispla
         // Everytime a key is pressed, we check to see if shift is held down
         // and update the shiftToggle flag accordingly
         updateShiftState(key, shiftToggle);
-        if (key == 259) {
+        if (key == KEY_BACKSPACE) {
             // Single Delete
             handleBackspace(inputBuffer, numChars);
         }
+        // else if (key == KEY_ENTER) {
+        //     TODO: for some reason if the first key pressed is enter then the cursor stops blinking in idle after
+        // }
         else {
             handleCharacterInput(key, inputBuffer, numChars, shiftToggle);
         }
         // printf("%s\n", inputBuffer);  // debug printf
-        // printf("%d\n", key);  // debug printf
+        printf("%d\n", key);  // debug printf
         // edge case for first time displaying a character
         if (*beginDisplay == 0) {
             *beginDisplay = 1;
@@ -90,13 +95,13 @@ void drawInput(Rectangle inputBox, char inputBuffer[], int *beginDisplay, Font f
     Vector2 startPos, endPos;
 
     if (*beginDisplay == 1) {
-        // start and end positions for the cursor when there is text on screen
+        // Start and end positions for the cursor when there is text on screen
         Vector2 textSize = MeasureTextEx(font, inputBuffer, fontSize, fontSpacing);
         startPos = (Vector2){ inputBox.x + textSize.x, inputBox.y };
         endPos = (Vector2){ inputBox.x + textSize.x, inputBox.y + textSize.y };
     }
     else {
-        // default position when there hasnt been any text on screen yet
+        // Default position when there hasnt been any text on screen yet
         startPos = (Vector2){ inputBox.x, inputBox.y };
         endPos = (Vector2){ inputBox.x, inputBox.y + fontSize };
     }
@@ -119,7 +124,7 @@ void drawInput(Rectangle inputBox, char inputBuffer[], int *beginDisplay, Font f
 
 int main() {
     InitWindow(screenWidth, screenHeight, "text-editor-bitch!");
-    SetTargetFPS(fpsLim);
+    SetTargetFPS(fpsCap);
 
     Font font = LoadFont("resources/Inter-Regular-slnt=0.ttf");
 
@@ -136,12 +141,22 @@ int main() {
     while (!WindowShouldClose()) {
         SetMouseCursor(MOUSE_CURSOR_IBEAM);
         int key = GetKeyPressed();
+        if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_O)) {
+            // Open a text file in write mode
+            FILE *pf = fopen("test.txt", "w");
+            fprintf(pf, "%s\n", inputBuffer);
+            fclose(pf);
+        }
         handleKeyPress(key, inputBuffer, &numChars, &beginDisplay, &shiftFlag, delTime);
         BeginDrawing();
-            ClearBackground(BLACK);
-            drawInput(inputBox, inputBuffer, &beginDisplay, font, key, blinkingClock);
+        ClearBackground(BLACK);
+        drawInput(inputBox, inputBuffer, &beginDisplay, font, key, blinkingClock);
         EndDrawing();
     }
+    // If accidently closed, then automatically write input buffer contents to the file
+    FILE *pf = fopen("test.txt", "w");
+    fprintf(pf, "%s", inputBuffer);
+    fclose(pf);
     CloseWindow();
     return 0;
 }
