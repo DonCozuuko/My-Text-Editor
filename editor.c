7496fa3c7c10d7fcf6141ac8819cd4ce3431c797
Don Cozuuko <leojamband@gmail.com>
2025-05-28 16:56:28 -0400

    Things done:  - Implemented line struct and cursor struct  - All text is now stored in an array of pointers to line structs    where each line struct contains a buffer of the text in that line    and the number of characters in that buffer.    Previously was stored in one long buffer with newlines seperating    every line.  - Cursor struct contains the current index position that cursor is on    (about to append to in the buffer for that line) and the current line    its on.  - handleEnter() doesnt append new lines to the buffer arrays anymore,    rather when displaying we just for loop iterate through all the line    buffers and offset them in the y axis, and when saving we automatically    append new lines when fprintf().  - So far only the left and right array keys move the cursor in the current line  - Error handling of seg faults for malloc() of line structs    and a for loop when the main while loop closes to clean up    allocated memory.
    TODO:
     - Want to have insert and delete logic for editing strings
       in the middle and not just the end.
     - Up and down arrows for cursor.
     - A read functionality to read files.

--- editor.c
+++ editor.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 #include <raylib.h>
+#include <stdlib.h>
+#include <string.h>
 #include "special_characters.h"
 
 static const int screenWidth = 960;
 static const int screenHeight = 540;
 static const int fpsCap = 60;
-static const int maxInputChars = 100;
+#define MAX_INPUT_CHARS 100
+#define maxNumLines 4
 static const float fontSize = 25.0;
 static const float fontSpacing = 2.0;
 static const int blinkRate = 40;            // how fast the cursor blinks
 static const int backspaceBuffer = 25;      // how long it takes to hold down backspace to rapidly backspace
 
+typedef struct {
+    char string[MAX_INPUT_CHARS + 1];
+    int numChars;
+} Line;
+
+typedef struct {
+    int indexPos;
+    int line;
+} Cursor;
+
 void updateShiftState(int key, int *shiftToggle) {
-    if (key == 340) {
+    if (key == KEY_LEFT_SHIFT || key == KEY_RIGHT_SHIFT) {
         *shiftToggle = 1;
     }
     if (!(IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_SHIFT))) {
@@ -20,42 +33,81 @@ void updateShiftState(int key, int *shiftToggle) {
     }
 }
 
-void handleBackspace(char inputBuffer[], int *numChars) {
-    inputBuffer[*numChars - 1] = '\0';
-    if (*numChars > 0) {
-        (*numChars)--;
+void handleBackspace(Line *inputBuffer[], int *currentLine, int *totalNumLines, Cursor *cursor) {
+    // int numberofChars = inputBuffer[*currentLine]->numChars;
+    if (cursor->indexPos > 0) {
+        inputBuffer[*currentLine]->string[cursor->indexPos - 1] = '\0';
+        inputBuffer[*currentLine]->numChars--;
+        cursor->indexPos--;
+    } 
+    else {
+        // Edge case for when you delete the last character in the first line
+        if (*currentLine != 0) {
+            free(inputBuffer[*currentLine]);
+            inputBuffer[*currentLine] = NULL;
+            (*currentLine)--;
+            (*totalNumLines)--;
+            int CharCount = inputBuffer[*currentLine]->numChars;
+            cursor->line--;
+            cursor->indexPos = CharCount;
+        }
     }
 }
 
-void formatChars(char inputBuffer[], int *numChars, int key) {
-    inputBuffer[*numChars] = (char)key;
-    inputBuffer[(*numChars) + 1] = '\0';
-    (*numChars)++;
+void formatChars(Line *inputBuffer[], int key, int currentLine, Cursor *cursor) {
+    // printf("%c", (char)key);
+    inputBuffer[currentLine]->string[cursor->indexPos] = (char)key;
+    inputBuffer[currentLine]->string[cursor->indexPos + 1] = '\0';
+    inputBuffer[currentLine]->numChars++;
 }
 
-void handleCharacterInput(int key, char inputBuffer[], int *numChars, int *shiftToggle) {
+void handleCharacterInput(int key, Line *inputBuffer[], int *shiftToggle, int currentLine, Cursor *cursor) {
     if (*shiftToggle == 0) {
         // Handles keys not on the shift layer
-        if (key >= KEY_A && KEY_Z <= 90) {
+        if (key >= KEY_A && key <= KEY_Z) {
             key += 32;
         }
     }
     else {
         // If shift button is being held
         handleSpecialChars(&key);
+        // printf("\nI should work here\n\n");
     }
     // Add the keys to the string array, but the keys must be printable ascii keys which excludes shift and cntrl
     if (key >= 32 && key <= 127) {
-        formatChars(inputBuffer, numChars, key);
+        formatChars(inputBuffer, key, currentLine, cursor);
+        // printf("\n%s\n\n", inputBuffer[currentLine]->string);
+    }
+    cursor->indexPos++;
+}
+
+void handleEnter(Line *inputBuffer[], int *currentLine, int *totalNumLines, Cursor *cursor) {
+    if (*totalNumLines < maxNumLines) {
+        (*currentLine)++;
+        (*totalNumLines)++;
+
+        if (inputBuffer[*currentLine] == NULL) {
+            inputBuffer[*currentLine] = malloc(sizeof(Line));
+            inputBuffer[*currentLine]->numChars = 0;
+            inputBuffer[*currentLine]->string[0] = '\0';
+        }
+        cursor->line++;
+        cursor->indexPos = 0;
+
+    } else {
+        // stderr is an error stream
+        fprintf(stderr, "\nYOU HAVE REACHED THE MAX NUMBER OF LINES\n\n");
+        return ;
     }
 }
 
-void handleKeyPress(int key, char inputBuffer[], int *numChars, int *beginDisplay, int *shiftToggle,  int delTime[]) {
+void handleKeyPress(int key, Line *inputBuffer[], int *beginDisplay, int *shiftToggle,  int delTime[], int *currentLine, int *totalNumLines, Cursor *cursor) {
     // Handles rapid deleting with a counter buffer
+    // printf("CL - %d\n", *currentLine);
     if (IsKeyDown(KEY_BACKSPACE)) {
         delTime[0]++;
         if (delTime[0] > backspaceBuffer) {
-            handleBackspace(inputBuffer, numChars);
+            handleBackspace(inputBuffer, currentLine, totalNumLines, cursor);
         }
     }
     else {
@@ -63,49 +115,69 @@ void handleKeyPress(int key, char inputBuffer[], int *numChars, int *beginDispla
     }
 
     if (key > 0) {
+        // edge case for first time displaying a character
+        if (*beginDisplay == 0) {
+            *beginDisplay = 1;
+        }
+        if (*currentLine < 0 || *currentLine >= maxNumLines) {
+            fprintf(stderr, "\nINVALID LINE INDEX BITCH\n\n");
+            return ;
+        }
         // Everytime a key is pressed, we check to see if shift is held down
         // and update the shiftToggle flag accordingly
         updateShiftState(key, shiftToggle);
         if (key == KEY_BACKSPACE) {
             // Single Delete
-            handleBackspace(inputBuffer, numChars);
+            handleBackspace(inputBuffer, currentLine, totalNumLines, cursor);
         }
-        // else if (key == KEY_ENTER) {
-        //     TODO: for some reason if the first key pressed is enter then the cursor stops blinking in idle after
-        // }
-        else {
-            handleCharacterInput(key, inputBuffer, numChars, shiftToggle);
+        else if (key == KEY_ENTER) {
+            handleEnter(inputBuffer, currentLine, totalNumLines, cursor);
         }
-        // printf("%s\n", inputBuffer);  // debug printf
-        printf("%d\n", key);  // debug printf
-        // edge case for first time displaying a character
-        if (*beginDisplay == 0) {
-            *beginDisplay = 1;
+        else if (key == KEY_LEFT) {
+            cursor->indexPos--;
         }
+        else if (key == KEY_RIGHT) {
+            cursor->indexPos++;
+        }
+        else if (key != KEY_LEFT_SHIFT && key != KEY_RIGHT_SHIFT){
+            handleCharacterInput(key, inputBuffer, shiftToggle, *currentLine, cursor);
+        }
+        // printf("%s\n", inputBuffer[currentLine]->string);  // debug printf
+        // printf("%d\n", key);  // debug printf
+        // printf("Indexpos - %d\n", cursor->indexPos);
+        // printf("LineNum - %d\n", cursor->line);
     }
+    // printf("\nIndexpos is %d\n", cursor->indexPos);
 }
 
-void drawInput(Rectangle inputBox, char inputBuffer[], int *beginDisplay, Font font, int key, int blinkingClock[]) {
-    if (*beginDisplay == 1) {
-        // Draw text input
-        Vector2 pos = { inputBox.x, inputBox.y };
-        DrawTextEx(font, inputBuffer, pos, fontSize, fontSpacing, WHITE);
-    }
+char *sliceStringByIndex(char *string, int endIndex) {
+    // start index is by default 0
+    // make a string copy, and slice that copy
+    // probably better to just make a Vector2 output of the MeasureTextEx using a sliced copy
+    char *copy = malloc(endIndex + 1);
+    strncpy(copy, string, endIndex);
+    copy[endIndex] = '\0';
+    return copy;
+}
 
+void drawCursor(int *beginDisplay, Rectangle inputBox, Line *inputBuffer[], int key, int blinkingClock[], Font font, Cursor *cursor, int currentLine) {
     Vector2 startPos, endPos;
-
     if (*beginDisplay == 1) {
         // Start and end positions for the cursor when there is text on screen
-        Vector2 textSize = MeasureTextEx(font, inputBuffer, fontSize, fontSpacing);
+        char *slicedString = sliceStringByIndex(inputBuffer[currentLine]->string, cursor->indexPos);
+        printf("%d\n", cursor->indexPos);
+        // printf("%s\n", inputBuffer[currentLine]->string);
+        Vector2 textSize = MeasureTextEx(font, slicedString, fontSize, fontSpacing);
         startPos = (Vector2){ inputBox.x + textSize.x, inputBox.y };
         endPos = (Vector2){ inputBox.x + textSize.x, inputBox.y + textSize.y };
+        free(slicedString);
     }
     else {
         // Default position when there hasnt been any text on screen yet
         startPos = (Vector2){ inputBox.x, inputBox.y };
         endPos = (Vector2){ inputBox.x, inputBox.y + fontSize };
     }
-
+    
     if (key > 0) {
         // Display solid cursor when typing
         blinkingClock[0] = 0;
@@ -122,41 +194,76 @@ void drawInput(Rectangle inputBox, char inputBuffer[], int *beginDisplay, Font f
     }
 }
 
+void drawInput(Rectangle inputBox, Line *inputBuffer[], int *beginDisplay, Font font, int key, int blinkingClock[], int currentLine, int totalNumLines, Cursor *cursor) {
+    if (*beginDisplay == 1) {
+        // Draw text input
+        Vector2 pos = { inputBox.x, inputBox.y };
+        for (int i = 0; i < totalNumLines; i++) {
+            DrawTextEx(font, inputBuffer[i]->string, pos, fontSize, fontSpacing, WHITE);
+            pos.y += fontSize + fontSpacing;
+        }
+    }
+
+    drawCursor(beginDisplay, inputBox, inputBuffer, key, blinkingClock, font, cursor, currentLine);
+}
+
 int main() {
     InitWindow(screenWidth, screenHeight, "text-editor-bitch!");
     SetTargetFPS(fpsCap);
 
     Font font = LoadFont("resources/Inter-Regular-slnt=0.ttf");
 
-    char inputBuffer[maxInputChars + 1];
+    Line *inputBuffer[maxNumLines] = { NULL };
 
     Rectangle inputBox = { 5, 0, 300, 25 };
 
-    int numChars = 0;      // counter
+    int totalNumLines = 1;
     int beginDisplay = 0;  // flag
     int shiftFlag = 0;     // flag
     int delTime[1] = {0};  // counter
     int blinkingClock[1] = {0};  // counter
+    int currentLine = 0;    // the current line number that cursor is
+
+    inputBuffer[currentLine] = malloc(sizeof(Line));
+    inputBuffer[currentLine]->numChars = 0;
+    inputBuffer[currentLine]->string[0] = '\0';
+
+    Cursor cursor = { 0, 1 };
 
     while (!WindowShouldClose()) {
-        SetMouseCursor(MOUSE_CURSOR_IBEAM);
         int key = GetKeyPressed();
         if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_O)) {
             // Open a text file in write mode
-            FILE *pf = fopen("test.txt", "w");
-            fprintf(pf, "%s\n", inputBuffer);
-            fclose(pf);
+            // FILE *pf = fopen("test.txt", "w");
+            // for (int i = 0; i < totalNumLines; i++) {
+            //     fprintf(pf, "%s\n", inputBuffer[i]->string);
+            // }
+            // fclose(pf);
         }
-        handleKeyPress(key, inputBuffer, &numChars, &beginDisplay, &shiftFlag, delTime);
+        handleKeyPress(key, inputBuffer, &beginDisplay, &shiftFlag, delTime, &currentLine, &totalNumLines, &cursor);
         BeginDrawing();
         ClearBackground(BLACK);
-        drawInput(inputBox, inputBuffer, &beginDisplay, font, key, blinkingClock);
+        DrawText("CTRL + O to save", 20, 520, 10, WHITE);
+        drawInput(inputBox, inputBuffer, &beginDisplay, font, key, blinkingClock, currentLine, totalNumLines, &cursor);
         EndDrawing();
     }
     // If accidently closed, then automatically write input buffer contents to the file
-    FILE *pf = fopen("test.txt", "w");
-    fprintf(pf, "%s", inputBuffer);
-    fclose(pf);
+    // FILE *pf = fopen("test.txt", "w");
+    // // new lines appended automatically to each line string
+    // for (int i = 0; i < totalNumLines; i++) {
+    //     fprintf(pf, "%s\n", inputBuffer[i]->string);
+    // }
+    // fclose(pf);
+    // Cleaning up
+    for (int i = 0; i < totalNumLines; i++) {
+        if (inputBuffer[i] != NULL) {
+            free(inputBuffer[i]);
+            inputBuffer[i] = NULL;
+            printf("\nLine %d has been freed\n\n", i);
+
+        }
+    }
+
     CloseWindow();
     return 0;
 }
