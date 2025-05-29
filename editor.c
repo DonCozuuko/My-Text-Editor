e5e0199c39ee2b97cbdda0f20c14cd1c4d87b4f0
Don Cozuuko <leojamband@gmail.com>
2025-05-29 13:37:35 -0400

    Things Done:  - Fixed the displayed cursor, problem was that it the y values    for the startPos and endPos vectors were not being adjusted    by the currentLine number, and after that the cursor was being    offset by the fontSpacing making it not uniform.  - Included insertion logic when both backspacing and typing    in strings where the cursor is in the middle of the string.  - Two edge cases for backspacing have been optimized, when    there is only one character in a line, after backspacing the cursor    immediately jumps to the line above it (without having to backspace twice),    and when there is only one character and only one line (line 0), the cursor wasnt    display before, but now it is.
    TODO:
     - Want to display line numbers like in vim.
     - Insertion and deletion of lines (e.g. with enter and backspacing)
       not coded yet.
     - Reading files.
     - Unit Tests so I can stop fucking doing printf() calls.
     - Grid & Zooming
     - Draw cursor on the new line when enter is pressed.

--- editor.c
+++ editor.c
@@ -8,7 +8,7 @@ static const int screenWidth = 960;
 static const int screenHeight = 540;
 static const int fpsCap = 60;
 #define MAX_INPUT_CHARS 100
-#define maxNumLines 4
+#define maxNumLines 10
 static const float fontSize = 25.0;
 static const float fontSpacing = 2.0;
 static const int blinkRate = 40;            // how fast the cursor blinks
@@ -24,6 +24,13 @@ typedef struct {
     int line;
 } Cursor;
 
+char *sliceStringByIndex(char *string, int startIndex, int endIndex) {
+    char *copy = malloc(endIndex + 1);
+    strncpy(copy, string + startIndex, endIndex);
+    copy[endIndex] = '\0';
+    return copy;
+}
+
 void updateShiftState(int key, int *shiftToggle) {
     if (key == KEY_LEFT_SHIFT || key == KEY_RIGHT_SHIFT) {
         *shiftToggle = 1;
@@ -33,31 +40,72 @@ void updateShiftState(int key, int *shiftToggle) {
     }
 }
 
-void handleBackspace(Line *inputBuffer[], int *currentLine, int *totalNumLines, Cursor *cursor) {
-    // int numberofChars = inputBuffer[*currentLine]->numChars;
-    if (cursor->indexPos > 0) {
-        inputBuffer[*currentLine]->string[cursor->indexPos - 1] = '\0';
-        inputBuffer[*currentLine]->numChars--;
-        cursor->indexPos--;
-    } 
-    else {
-        // Edge case for when you delete the last character in the first line
-        if (*currentLine != 0) {
-            free(inputBuffer[*currentLine]);
-            inputBuffer[*currentLine] = NULL;
-            (*currentLine)--;
-            (*totalNumLines)--;
-            int CharCount = inputBuffer[*currentLine]->numChars;
-            cursor->line--;
-            cursor->indexPos = CharCount;
-        }
-    }
+void backspaceEndOfString(Line *inputBuffer[], int currentLine, Cursor *cursor) {
+    inputBuffer[currentLine]->string[cursor->indexPos - 1] = '\0';
+    inputBuffer[currentLine]->numChars--;
+    cursor->indexPos--;
 }
 
-void formatChars(Line *inputBuffer[], int key, int currentLine, Cursor *cursor) {
-    // printf("%c", (char)key);
+void backspaceMiddleOfString(Line *inputBuffer[], int currentLine, Cursor *cursor) {
+    char *slicedString = sliceStringByIndex(inputBuffer[currentLine]->string, cursor->indexPos, inputBuffer[currentLine]->numChars);
+    cursor->indexPos--;
+    inputBuffer[currentLine]->string[cursor->indexPos] = '\0';
+    for (int i = 0; i < strlen(slicedString); i++) {
+        inputBuffer[currentLine]->string[cursor->indexPos + i] = slicedString[i]; 
+    }
+    inputBuffer[currentLine]->string[cursor->indexPos + strlen(slicedString)] = '\0';
+    inputBuffer[currentLine]->numChars--;
+}
+
+void handleBackspace(Line *inputBuffer[], int *currentLine, int *totalNumLines, Cursor *cursor, int *beginDisplay) {
+    // Handling backspaces when the cursor's index position is at the end of the string
+    if (cursor->indexPos > 0 && cursor->indexPos == inputBuffer[*currentLine]->numChars) {
+        // Handling backspaces when the string is only 1 character long
+        if (inputBuffer[*currentLine]->numChars == 1) {
+            // Handling backspaces when the currentline is not the first line (0)
+            if (*currentLine != 0) {
+                free(inputBuffer[*currentLine]);
+                inputBuffer[*currentLine] = NULL;
+                (*currentLine)--;
+                (*totalNumLines)--;
+                cursor->line = 0;
+                cursor->indexPos = inputBuffer[*currentLine]->numChars;
+            }
+            else {
+                inputBuffer[*currentLine]->string[0] = '\0';
+                inputBuffer[*currentLine]->numChars = 0;
+                cursor->indexPos--;
+                *beginDisplay = 0;
+            }
+        }
+        else {
+            backspaceEndOfString(inputBuffer, *currentLine, cursor);
+        }
+    }
+    // 
+    else if (cursor->indexPos > 0 && cursor->indexPos < inputBuffer[*currentLine]->numChars) {
+        backspaceMiddleOfString(inputBuffer, *currentLine, cursor);
+    }
+    // else if (cursor->indexPos == 0) {
+    //     ;
+    // }
+}
+
+void formatCharsAppend(Line *inputBuffer[], int key, int currentLine, Cursor *cursor) {
     inputBuffer[currentLine]->string[cursor->indexPos] = (char)key;
     inputBuffer[currentLine]->string[cursor->indexPos + 1] = '\0';
+    inputBuffer[currentLine]->numChars++;
+    cursor->indexPos++;
+}
+
+void formatCharsMiddle(Line *inputBuffer[], int key, int currentLine, Cursor *cursor) {
+    char *slicedString = sliceStringByIndex(inputBuffer[currentLine]->string, cursor->indexPos, inputBuffer[currentLine]->numChars);
+    inputBuffer[currentLine]->string[cursor->indexPos] = (char)key;
+    cursor->indexPos++;
+    for (int i = 0; i < strlen(slicedString); i++) {
+        inputBuffer[currentLine]->string[cursor->indexPos + i] = slicedString[i];
+    }
+    inputBuffer[currentLine]->string[cursor->indexPos + strlen(slicedString)] = '\0';
     inputBuffer[currentLine]->numChars++;
 }
 
@@ -71,14 +119,18 @@ void handleCharacterInput(int key, Line *inputBuffer[], int *shiftToggle, int cu
     else {
         // If shift button is being held
         handleSpecialChars(&key);
-        // printf("\nI should work here\n\n");
     }
     // Add the keys to the string array, but the keys must be printable ascii keys which excludes shift and cntrl
     if (key >= 32 && key <= 127) {
-        formatChars(inputBuffer, key, currentLine, cursor);
-        // printf("\n%s\n\n", inputBuffer[currentLine]->string);
+        if (cursor->indexPos == inputBuffer[currentLine]->numChars) {
+            // When appending to the string array (i.e. editing at the end of the string)
+            formatCharsAppend(inputBuffer, key, currentLine, cursor);
+        }
+        else {
+            // Editing the string on an index position within the string
+            formatCharsMiddle(inputBuffer, key, currentLine, cursor);
+        }
     }
-    cursor->indexPos++;
 }
 
 void handleEnter(Line *inputBuffer[], int *currentLine, int *totalNumLines, Cursor *cursor) {
@@ -103,11 +155,10 @@ void handleEnter(Line *inputBuffer[], int *currentLine, int *totalNumLines, Curs
 
 void handleKeyPress(int key, Line *inputBuffer[], int *beginDisplay, int *shiftToggle,  int delTime[], int *currentLine, int *totalNumLines, Cursor *cursor) {
     // Handles rapid deleting with a counter buffer
-    // printf("CL - %d\n", *currentLine);
     if (IsKeyDown(KEY_BACKSPACE)) {
         delTime[0]++;
         if (delTime[0] > backspaceBuffer) {
-            handleBackspace(inputBuffer, currentLine, totalNumLines, cursor);
+            handleBackspace(inputBuffer, currentLine, totalNumLines, cursor, beginDisplay);
         }
     }
     else {
@@ -128,21 +179,27 @@ void handleKeyPress(int key, Line *inputBuffer[], int *beginDisplay, int *shiftT
         updateShiftState(key, shiftToggle);
         if (key == KEY_BACKSPACE) {
             // Single Delete
-            handleBackspace(inputBuffer, currentLine, totalNumLines, cursor);
+            handleBackspace(inputBuffer, currentLine, totalNumLines, cursor, beginDisplay);
         }
         else if (key == KEY_ENTER) {
             handleEnter(inputBuffer, currentLine, totalNumLines, cursor);
         }
         else if (key == KEY_LEFT) {
             cursor->indexPos--;
+            if (cursor->indexPos < 0) {
+                cursor->indexPos = 0;
+            }
         }
         else if (key == KEY_RIGHT) {
             cursor->indexPos++;
+            if (cursor->indexPos > inputBuffer[*currentLine]->numChars) {
+                cursor->indexPos = inputBuffer[*currentLine]->numChars;
+            }
         }
         else if (key != KEY_LEFT_SHIFT && key != KEY_RIGHT_SHIFT){
             handleCharacterInput(key, inputBuffer, shiftToggle, *currentLine, cursor);
         }
-        // printf("%s\n", inputBuffer[currentLine]->string);  // debug printf
+        printf("%s\n", inputBuffer[*currentLine]->string);  // debug printf
         // printf("%d\n", key);  // debug printf
         // printf("Indexpos - %d\n", cursor->indexPos);
         // printf("LineNum - %d\n", cursor->line);
@@ -150,26 +207,15 @@ void handleKeyPress(int key, Line *inputBuffer[], int *beginDisplay, int *shiftT
     // printf("\nIndexpos is %d\n", cursor->indexPos);
 }
 
-char *sliceStringByIndex(char *string, int endIndex) {
-    // start index is by default 0
-    // make a string copy, and slice that copy
-    // probably better to just make a Vector2 output of the MeasureTextEx using a sliced copy
-    char *copy = malloc(endIndex + 1);
-    strncpy(copy, string, endIndex);
-    copy[endIndex] = '\0';
-    return copy;
-}
-
 void drawCursor(int *beginDisplay, Rectangle inputBox, Line *inputBuffer[], int key, int blinkingClock[], Font font, Cursor *cursor, int currentLine) {
     Vector2 startPos, endPos;
     if (*beginDisplay == 1) {
         // Start and end positions for the cursor when there is text on screen
-        char *slicedString = sliceStringByIndex(inputBuffer[currentLine]->string, cursor->indexPos);
-        printf("%d\n", cursor->indexPos);
-        // printf("%s\n", inputBuffer[currentLine]->string);
+        int startIndex = 0;
+        char *slicedString = sliceStringByIndex(inputBuffer[currentLine]->string, startIndex, cursor->indexPos);
         Vector2 textSize = MeasureTextEx(font, slicedString, fontSize, fontSpacing);
-        startPos = (Vector2){ inputBox.x + textSize.x, inputBox.y };
-        endPos = (Vector2){ inputBox.x + textSize.x, inputBox.y + textSize.y };
+        startPos = (Vector2){ inputBox.x + textSize.x, inputBox.y + (textSize.y * (currentLine))};
+        endPos = (Vector2){ inputBox.x + textSize.x, inputBox.y + (textSize.y * (currentLine + 1))};
         free(slicedString);
     }
     else {
@@ -200,7 +246,7 @@ void drawInput(Rectangle inputBox, Line *inputBuffer[], int *beginDisplay, Font 
         Vector2 pos = { inputBox.x, inputBox.y };
         for (int i = 0; i < totalNumLines; i++) {
             DrawTextEx(font, inputBuffer[i]->string, pos, fontSize, fontSpacing, WHITE);
-            pos.y += fontSize + fontSpacing;
+            pos.y += fontSize;
         }
     }
 
@@ -213,6 +259,9 @@ int main() {
 
     Font font = LoadFont("resources/Inter-Regular-slnt=0.ttf");
 
+    if (font.baseSize == 0) {
+        fprintf(stderr, "\nFailed to load font!\n\n");
+    }
     Line *inputBuffer[maxNumLines] = { NULL };
 
     Rectangle inputBox = { 5, 0, 300, 25 };
@@ -229,17 +278,18 @@ int main() {
     inputBuffer[currentLine]->string[0] = '\0';
 
     Cursor cursor = { 0, 1 };
-
+    // TODO: HAVE A KEYBIND TO DISPLAY A GRID LIKE IN EXAMPLE VID
+    // TODO: BE ABLE TO CTRL +, CTRL -, TO ZOOM IN AND ZOOM OUT TO SHRINK AND ENLARGE TEXT/GRID
     while (!WindowShouldClose()) {
         int key = GetKeyPressed();
-        if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_O)) {
-            // Open a text file in write mode
-            // FILE *pf = fopen("test.txt", "w");
-            // for (int i = 0; i < totalNumLines; i++) {
-            //     fprintf(pf, "%s\n", inputBuffer[i]->string);
-            // }
-            // fclose(pf);
-        }
+        // if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_O)) {
+        //     // Open a text file in write mode
+        //     // FILE *pf = fopen("test.txt", "w");
+        //     // for (int i = 0; i < totalNumLines; i++) {
+        //     //     fprintf(pf, "%s\n", inputBuffer[i]->string);
+        //     // }
+        //     // fclose(pf);
+        // }
         handleKeyPress(key, inputBuffer, &beginDisplay, &shiftFlag, delTime, &currentLine, &totalNumLines, &cursor);
         BeginDrawing();
         ClearBackground(BLACK);
